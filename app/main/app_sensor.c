a1c514f2e475578ef1d3054d9e79e991d43c4c44
ubuntuT480 <wanghong2016@foxmail.com>
2022-05-15 19:41:31 +0800

    add volt sensor

--- app/main/app_sensor.c
+++ app/main/app_sensor.c
@@ -4,12 +4,37 @@
 #include "freertos/timers.h"
 
 #include "esp_log.h"
+#include "driver/adc.h"
 
 #include "app_sensor.h"
 
 static const char *SENSORTAG = "app_sensor";
 static QueueHandle_t sensor_QueueHandle_t = NULL;
 static int sensor_timer_cnt=0;
+
+static void bsp_volt_init(void)
+{
+    // 1. init adc
+    adc_config_t adc_config;
+
+    // Depend on menuconfig->Component config->PHY->vdd33_const value
+    // When measuring system voltage(ADC_READ_VDD_MODE), vdd33_const must be set to 255.
+    adc_config.mode = ADC_READ_TOUT_MODE;
+    adc_config.clk_div = 8; // ADC sample collection clock = 80MHz/clk_div = 10MHz
+    ESP_ERROR_CHECK(adc_init(&adc_config));
+}
+
+static void read_volt(void)
+{
+    uint16_t adc_data[10];
+    if (ESP_OK == adc_read_fast(adc_data, 10)) {
+        ESP_LOGI(SENSORTAG, "volt: %d", (adc_data[0]+adc_data[9])/2);
+    }
+    else
+    {
+        ESP_LOGE(SENSORTAG, "read volt fail");
+    }
+}
 
 static void sensor_timer_callback(TimerHandle_t xTimer)
 {
@@ -21,11 +46,11 @@ static void sensor_timer_callback(TimerHandle_t xTimer)
     }
 
     /*chenck temp sensor*/
-    if(sensor_timer_cnt % (SENSOR_TRIGGER_TEMP_TICKS/SENSOR_TRIGGER_MIN_TICKS) == 0)
+    if(sensor_timer_cnt % (SENSOR_TRIGGER_200ms_TICKS/SENSOR_TRIGGER_MIN_TICKS) == 0)
     {
-        test.sensor_id = sensor_timer_cnt;
+        test.sensor_id = SENSOR_ID_VOLT;
         xQueueSend(sensor_QueueHandle_t,&test,0);
-        test.sensor_id = 1;
+        test.sensor_id = SENSOR_ID_TEMP;
         xQueueSend(sensor_QueueHandle_t,&test,0);
     }
 }
@@ -38,6 +63,10 @@ static void sensor_task(void* pvParameters)
         /* read sensor read event queue */
         xQueueReceive(sensor_QueueHandle_t,&test,portMAX_DELAY);
         ESP_LOGI(SENSORTAG, "sensor id: %d", test.sensor_id);
+        if(test.sensor_id == SENSOR_ID_VOLT)
+        {
+            read_volt();
+        }
     }
     
     vTaskDelete(NULL);
@@ -46,6 +75,10 @@ static void sensor_task(void* pvParameters)
 void sensor_task_init()
 {
     int err=0;
+    /*sensor bsp init*/
+    bsp_volt_init();
+
+
     /*create sensor read event queue*/
     sensor_QueueHandle_t = xQueueCreate(SENSOR_QUEUE_LEN, sizeof(struct SensorEvent));
     if(!sensor_QueueHandle_t)
--- app/main/app_sensor.h
+++ app/main/app_sensor.h
@@ -2,10 +2,15 @@
 #define __APP_SENSOR_H
 
 #define SENSOR_TRIGGER_MAX_TICKS (1000) 
-#define SENSOR_TRIGGER_TEMP_TICKS (200) 
+#define SENSOR_TRIGGER_200ms_TICKS (200) 
 #define SENSOR_TRIGGER_MIN_TICKS (10) 
 #define SENSOR_TRIGGER_TIMER_ID  (1)
 #define SENSOR_QUEUE_LEN         (10)
+
+enum{
+    SENSOR_ID_VOLT=0,
+    SENSOR_ID_TEMP,
+};
 
 struct SensorEvent{
     int sensor_id;
